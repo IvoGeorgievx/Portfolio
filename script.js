0d073cc3614889cd6cb0fe8a23bf970ba2b0a1e8
IvoGeorgievx <102153817+IvoGeorgievx@users.noreply.github.com>
2025-03-24 19:21:50 +0200

    added sections

--- script.js
+++ script.js
@@ -13,14 +13,28 @@ const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((element) => observer.observe(element));
 
 const hobbies = [
-	{ name: "Reading", description: "Something something" },
-	{ name: "Sports", description: "Gym" },
-	{ name: "History", description: "WWII" },
-	{ name: "Hiking", description: "Mountains" },
+	{
+		description:
+			"Mostly enjoying reading biographies, memoirs and fiction. My favorite books are: 'All quiet on the western front', 'The Forgotten Soldier'",
+	},
+	{
+		description:
+			"I'm enjoying hiking in the mountains and even though there aren't many around where I live, I tend to go few times a year.",
+	},
+	{
+		description:
+			"History wise I enjoy reading and learning modern history. Mostly both world wars.",
+	},
+	{
+		description:
+			"I practice all kinds of sports like cycling, running, volleyball, but mostly gym and football.",
+	},
 ];
 
 const content = document.getElementById("interest-content");
 const tabs = document.querySelectorAll(".interest-tab");
+const backgroundDiv = document.createElement("div");
+backgroundDiv.classList.add("interest-content-background");
 tabs.forEach((tab) => {
 	tab.addEventListener("click", () => {
 		tabs.forEach((t) => t.classList.remove("active"));
@@ -28,9 +42,9 @@ tabs.forEach((tab) => {
 
 		const index = tab.getAttribute("data-index");
 		content.innerHTML = `
-                    <span>${hobbies[index].name}</span>
                     <span>${hobbies[index].description}</span>
                 `;
+		content.appendChild(backgroundDiv);
 	});
 });
 
